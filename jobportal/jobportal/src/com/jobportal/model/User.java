1780470258ab6a3c0443b2e79253ecd247b2119e
sahan200401 <jbasnethmina@gmail.com>
2026-01-18 20:19:30 +0530

    adding user.java class

--- jobportal/jobportal/src/com/jobportal/model/User.java
+++ jobportal/jobportal/src/com/jobportal/model/User.java
@@ -1,4 +1,105 @@
 package com.jobportal.model;
 
+import javax.management.relation.Role;
+import java.time.LocalDateTime;
+
 public class User {
+    private int userid;
+    private String username;
+    private String password;
+    private String email;
+    private Role role;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public enum role{
+        CANDIDATE, EMPLOYER, ADMIN
+    }
+
+    public User() {
+    }
+
+    public User(String username, String password, String email, Role role) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+    }
+
+    public User(int userid, String username, String password, String email, Role role) {
+        this.userid = userid;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+    }
+
+    public int getUserid() {
+        return userid;
+    }
+
+    public void setUserid(int userid) {
+        this.userid = userid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userid=" + userid +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", role=" + role +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                '}';
+    }
 }
